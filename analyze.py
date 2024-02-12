c5b09cc2dd8ac51c11e978b866497716ddc0935b
Yi <yw6157@nyu.edu>
2024-02-11 23:08:59 -0500

    Finished the two python codes

--- analyze.py
+++ analyze.py
@@ -1,1 +1,47 @@
 # Place code below to do the analysis part of the assignment.
+f = open('./data/clean_data.csv', 'r')
+lines = f.readlines()
+
+last = int(lines[-1].strip().split(',')[0])
+sums = 0
+average = 0
+count = 1880
+firstc = 0
+lastc = 0
+dic = {}
+dic1 = {}
+
+for i in range(len(lines)-1):
+    values = lines[i+1].strip().split(',')
+    for j in range(12):
+        sums += float(values[j+1])
+    average = format(sums/12, ".1f")
+    sums = 0
+    dic[values[0]] = average
+# print(last)
+
+for j in range(len(dic)//10):
+    firstc = count
+    for i in range(10):
+        sums += float(dic[str(count)])
+        count += 1
+    lastc = count - 1
+    average = format(sums/10, ".1f")
+    sums = 0
+    dic1[str(firstc) + " to " + str(lastc)] = average
+    average = 0
+
+if count != last:
+    firstc = count
+    lastc = last
+    c = 0
+    while count != last:
+        sums += float(dic[str(count)])
+        count += 1
+        c += 1
+
+    average = format(sums/c, ".1f")
+    dic1[str(firstc) + " to " + str(lastc)] = average
+
+for key, value in dic1.items():
+    print("The average temperature from " + key + " is " + value + ".")
--- munge.py
+++ munge.py
@@ -1,1 +1,54 @@
 # Place code below to do the munging part of this assignment.
+f = open('./data/raw.txt','r')
+lines = f.readlines()
+newline = []
+count = 0
+
+for i in range(len(lines)):
+    if count == 0 and lines[i].startswith("Year"):
+        newline.append(lines[i])
+        count += 1
+    elif lines[i].startswith("1") or lines[i].startswith("2"):
+        newline.append(lines[i])
+
+cleanline = []
+cleandata = ""
+for i in range(len(newline)):
+    for j in range(len(newline[i])):
+        if newline[i][j] != " ":
+            cleandata += newline[i][j]
+        else:
+            if newline[i][j+1] != " ":
+                cleandata += ","
+    cleanline.append(cleandata)
+    cleandata = ""
+
+cleanline1 = []
+cleandata1 = ""
+cleanline1.append(cleanline[0])
+
+for i in range(len(cleanline)-1):
+    values = cleanline[i+1].strip().split(',')
+    cleandata1 += values[0] + ","
+    for j in range(len(values)-2):
+        if values[j+1] != '***' and values[j+1] != '****':
+            newvalue = format(float(values[j+1])/100*1.8,".1f")
+            cleandata1 += newvalue + ","
+        else:
+            cleandata1 += values[j+1] + ","
+    cleandata1 += values[-1] + "\n"
+    cleanline1.append(cleandata1)
+    cleandata1 = ""
+
+# print(cleanline1)
+newf = open('./data/clean_data.csv', 'w')
+
+for i in range(len(cleanline1)):
+    newf.write(cleanline1[i])
+
+
+
+
+
+
+    
